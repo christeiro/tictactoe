4afbe76f222cf3b246ba5f3555d621ec247f36e3
Krister Vals <krister.vals@gmail.com>
2015-07-21 18:42:05 +0300

    Variables more readable

--- ticktacktoe.rb
+++ ticktacktoe.rb
@@ -29,25 +29,25 @@ def initialize_board
   b
 end
 
-def draw_board(b)
+def draw_board(board)
   system "clear"
-  puts " #{b[1]} | #{b[2]} | #{b[3]} " 
+  puts " #{board[1]} | #{board[2]} | #{board[3]} " 
   puts "---+---+---"
-  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "---+---+---"
-  puts " #{b[7]} | #{b[8]} | #{b[9]} "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
 
-def empty_positions(b)
-  b.select { |k, v| v == ' ' }.keys 
+def empty_positions(board)
+  board.select { |k, v| v == ' ' }.keys 
 end
 
-def player_picks_square(b)
+def player_picks_square(board)
   loop do
     puts "Pick a square (1 - 9):"
     position = gets.chomp.to_i
-    if empty_positions(b).include?(position)
-      b[position] = 'X'
+    if empty_positions(board).include?(position)
+      board[position] = 'X'
       break
     else
       puts "This position is already taken"
@@ -55,10 +55,10 @@ def player_picks_square(b)
   end
 end
 
-def computer_picks_square(b)
+def computer_picks_square(board)
   position = nil;
   WINNING_LINES.each do |line|
-    possible_winning_lines = Hash[ line.collect { |v| [v, b[v]] }]
+    possible_winning_lines = Hash[ line.collect { |v| [v, board[v]] }]
     next_move = two_in_a_row(possible_winning_lines,'X')
     if next_move
       position = next_move
@@ -66,13 +66,13 @@ def computer_picks_square(b)
     end
   end
     # Not sure if the following is the best solution, comes from the old programming habbit 
-    b[position ? position : empty_positions(b).sample] = '0'
+    board[position ? position : empty_positions(board).sample] = '0'
 end
 
-def check_winner(b)
+def check_winner(board)
   WINNING_LINES.each do |line|
-    return 'Player' if b.values_at(*line).count('X') == 3
-    return 'Computer' if b.values_at(*line).count('0') == 3
+    return 'Player' if board.values_at(*line).count('X') == 3
+    return 'Computer' if board.values_at(*line).count('0') == 3
   end
   nil
 end
@@ -88,15 +88,15 @@ def two_in_a_row(hsh, mrkr)
 end
 
 
-board = initialize_board
-draw_board(board)
+game_board = initialize_board
+draw_board(game_board)
 
 begin
-  player_picks_square(board)
-  computer_picks_square(board)
-  draw_board(board)
-  winner = check_winner(board)
-end until winner || empty_positions(board).empty?
+  player_picks_square(game_board)
+  computer_picks_square(game_board)
+  draw_board(game_board)
+  winner = check_winner(game_board)
+end until winner || empty_positions(game_board).empty?
 
 if winner
   puts "#{winner} won"
